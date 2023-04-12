0268532eabeb8b7520d1412e3ce9f22cd3302b1f
sonnl <sonnl@savisgroup.vn>
2023-04-12 18:04:11 +0700

    sub route navigation

--- lib/src/features/home_app_bar/shopping_cart_icon.dart
+++ lib/src/features/home_app_bar/shopping_cart_icon.dart
@@ -1,6 +1,6 @@
-import 'package:ecommerce_app/src/features/shopping_cart/shopping_cart_screen.dart';
-import 'package:flutter/material.dart';
 import 'package:ecommerce_app/src/constants/app_sizes.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 /// Shopping cart icon with items count badge
 class ShoppingCartIcon extends StatelessWidget {
@@ -19,12 +19,7 @@ class ShoppingCartIcon extends StatelessWidget {
           child: IconButton(
             key: shoppingCartIconKey,
             icon: const Icon(Icons.shopping_cart),
-            onPressed: () => Navigator.of(context).push(
-              MaterialPageRoute(
-                fullscreenDialog: true,
-                builder: (_) => const ShoppingCartScreen(),
-              ),
-            ),
+            onPressed: () => GoRouter.of(context).go('/cart'),
           ),
         ),
         if (cartItemsCount > 0)
@@ -62,7 +57,7 @@ class ShoppingCartIconBadge extends StatelessWidget {
           textScaleFactor: 1.0,
           style: Theme.of(context)
               .textTheme
-              .caption!
+              .bodySmall!
               .copyWith(color: Colors.white),
         ),
       ),
