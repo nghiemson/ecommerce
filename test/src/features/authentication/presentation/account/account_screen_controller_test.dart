0064f728c8ba5a4762569bb9d1f70428a2991f83
sonnl <sonnghiemaz@gmail.com>
2023-05-09 00:22:35 +0700

    test acc screen controller

--- test/src/features/authentication/presentation/account/account_screen_controller_test.dart
+++ test/src/features/authentication/presentation/account/account_screen_controller_test.dart
@@ -1,7 +1,14 @@
+import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
+import 'package:ecommerce_app/src/features/authentication/presentation/account/account_screen_controller.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('AccountScreenController', () {
-
+    test('initial state is AsyncValue.data', () {
+      final authRepo = FakeAuthRepository();
+      final controller = AccountScreenController(authRepository: authRepo);
+      expect(controller.debugState, const AsyncData<void>(null));
+    });
   });
 }
