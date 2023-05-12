d49713da2ca62295e6c2a160647da3626c9fb24b
sonnl <sonnl@savisgroup.vn>
2023-05-12 18:00:08 +0700

    test streamMatcher

--- test/src/features/authentication/presentation/account/account_screen_controller_test.dart
+++ test/src/features/authentication/presentation/account/account_screen_controller_test.dart
@@ -4,15 +4,14 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
-class MockAuthRepository extends Mock implements FakeAuthRepository{
-
-}
+class MockAuthRepository extends Mock implements FakeAuthRepository {}
 
 void main() {
   group('AccountScreenController', () {
     test('initial state is AsyncValue.data', () {
       final authRepository = MockAuthRepository();
-      final controller = AccountScreenController(authRepository: authRepository);
+      final controller =
+          AccountScreenController(authRepository: authRepository);
       expect(controller.debugState, const AsyncData<void>(null));
       verifyNever(authRepository.signOut);
     });
@@ -21,12 +20,35 @@ void main() {
       //setup
       final authRepository = MockAuthRepository();
       when(authRepository.signOut).thenAnswer((_) => Future.value());
-      final controller = AccountScreenController(authRepository: authRepository);
+      final controller =
+          AccountScreenController(authRepository: authRepository);
+      expectLater(
+          controller.stream,
+          emitsInOrder(const [
+            AsyncLoading<void>(),
+            AsyncData<void>(null),
+          ]));
       //run
       await controller.signOut();
       //verify
       verify(authRepository.signOut).called(1);
-      expect(controller.debugState, const AsyncData<void>(null));
+    },
+      timeout: const Timeout(Duration(microseconds: 500)),
+    );
+
+    test('signOut failure', () async {
+      //setup
+      final authRepository = MockAuthRepository();
+      final exception = Exception('Connection failed');
+      when(authRepository.signOut).thenThrow(exception);
+      final controller =
+          AccountScreenController(authRepository: authRepository);
+      //run
+      await controller.signOut();
+      //verify
+      verify(authRepository.signOut).called(1);
+      expect(controller.debugState.hasError, true);
+      expect(controller.debugState, isA<AsyncError>());
     });
   });
 }
