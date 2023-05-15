59df6bd47c3bc351823fde56e0febc3594959dcc
sonnl <sonnghiemaz@gmail.com>
2023-05-16 00:13:01 +0700

    add email and password widget tests

--- test/src/features/authentication/auth_robot.dart
+++ test/src/features/authentication/auth_robot.dart
@@ -1,6 +1,9 @@
 import 'package:ecommerce_app/src/common_widgets/alert_dialogs.dart';
+import 'package:ecommerce_app/src/common_widgets/primary_button.dart';
 import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:ecommerce_app/src/features/authentication/presentation/account/account_screen.dart';
+import 'package:ecommerce_app/src/features/authentication/presentation/sign_in/email_password_sign_in_screen.dart';
+import 'package:ecommerce_app/src/features/authentication/presentation/sign_in/email_password_sign_in_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -9,14 +12,44 @@ import '../../mocks.dart';
 
 class AuthRobot {
   AuthRobot(this.tester);
+
   final WidgetTester tester;
+
+  Future<void> pumpEmailPasswordSignInContents({
+    required FakeAuthRepository authRepository,
+    required EmailPasswordSignInFormType formType,
+    VoidCallback? onSignedIn,
+  }) {
+    return tester.pumpWidget(
+      ProviderScope(
+        overrides: [
+          authRepositoryProvider.overrideWithValue(authRepository),
+        ],
+        child: MaterialApp(
+          home: Scaffold(
+            body: EmailPasswordSignInContents(
+              formType: formType,
+              onSignedIn: onSignedIn,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> tapEmailAndPasswordSubmitButton() async {
+    final primaryButton = find.byType(PrimaryButton);
+    expect(primaryButton, findsOneWidget);
+    await tester.tap(primaryButton);
+    await tester.pump();
+  }
 
   Future<void> pumpAccountScreen({FakeAuthRepository? authRepository}) async {
     await tester.pumpWidget(
-       ProviderScope(
+      ProviderScope(
         overrides: [
-          if(authRepository != null)
-          authRepositoryProvider.overrideWithValue(authRepository),
+          if (authRepository != null)
+            authRepositoryProvider.overrideWithValue(authRepository),
         ],
         child: const MaterialApp(
           home: AccountScreen(),
@@ -64,5 +97,10 @@ class AuthRobot {
   void expectErrorAlertNotFound() {
     final finder = find.text('Error');
     expect(finder, findsNothing);
+  }
+
+  void expectCircularProgressIndicator() {
+    final finder = find.byType(CircularProgressIndicator);
+    expect(finder, findsOneWidget);
   }
 }
